8e95785a0621b13b1ef369ad6da9a47a2129c2db
zxwanderer <no@mail.com>
2023-02-18 22:34:38 +0300

    update

--- src/_code.asm
+++ src/_code.asm
@@ -2,13 +2,12 @@ start:
   ; LD HL, music_startgame.data
   ; CALL Tritone.play
 
-loop:
   DI
+  CALL SCREEN_CLEAR
   LD HL, MAP_SET
   CALL COPY_TO_BUFFER
 	CALL TILE16_SHOW_SCREEN
  EI
 
+loop:
   jp loop
-
-index: db 0
