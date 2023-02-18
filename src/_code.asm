4e2b4ec6cbf8be100283a1ea9eda0ca32279c0a6
zxwanderer <no@mail.com>
2023-02-18 18:20:46 +0300

    update

--- src/_code.asm
+++ src/_code.asm
@@ -2,13 +2,9 @@ start:
   LD HL, music_startgame.data
   CALL Tritone.play
 
+	CALL TILE16_SHOW_SCREEN
+
 loop:
-  LD A, (index)
-  INC A
-  LD (index), A
-  CALL TILE16_INDEX_TO_PTR
-  LD DE, #4000
-	CALL TILE_16_SHOW
   jp loop
 
 index: db 0
