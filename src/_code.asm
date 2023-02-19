48bb495c35e311f15419758268dc06b973bda8b0
zxwanderer <no@mail.com>
2023-02-19 11:36:22 +0300

    updates

--- src/_code.asm
+++ src/_code.asm
@@ -1,11 +1,16 @@
+  include "im2/im2_h.asm"
+  include "stack/push_pop_h.asm"
+
 start:
+  DI
+  SetIM2 interruptTab, INT_VECTOR
   ; LD HL, music_startgame.data
   ; CALL Tritone.play
+  EI
 
-  DI
   CALL SCREEN_CLEAR
-  LD HL, MAP_SET
-  CALL COPY_TO_BUFFER
+  ; LD HL, MAP_SET
+  ; CALL COPY_TO_BUFFER
 	CALL TILE16_SHOW_SCREEN
  EI
 
---
+++ src/_im2_routines.asm
@@ -0,0 +1,10 @@
+interr:	
+	di
+	DO_PUSH_ALL_REGISTRY
+	
+  LD A,1
+  OUT(#FE), A
+
+	DO_POP_ALL_REGISTRY
+	ei
+	ret
