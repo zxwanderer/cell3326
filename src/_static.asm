754febb25638db00c340e79ecb071dc37532b386
zxwanderer <no@mail.com>
2023-02-19 23:05:04 +0300

    update

--- src/_static.asm
+++ src/_static.asm
@@ -3,15 +3,21 @@
 
 	include "screen/addr_to_attr.asm"
 	include "screen/clear.asm"
+	include "screen/calc_down_pos.asm"
+	include "screen/set_colors.asm"
 
 	include "tiles16/show.asm"
 	include "tiles16/index_to_ptr.asm"
 	include "tiles16/show_screen.asm"
   include "tiles16/copy_to_buffer.asm"
 
+	include "text/text68.asm"
+
 	include "keyboard/wait_key.asm"
 	include "keyboard/wait_unkey.asm"
 
+  include "lang/lang_ru.asm"
+  
   MODULE music_startgame
 data:  
     include "music/AER/foryou.asm"
