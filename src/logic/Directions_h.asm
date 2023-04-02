932563a3785af219dd783d27e29bc1900b5610b8
zxwanderer <no@mail.com>
2023-04-02 18:01:23 +0300

    some restyle

--- src/logic/Directions_h.asm
+++ src/logic/Directions_h.asm
@@ -7,12 +7,3 @@ dir_up_right EQU 5
 dir_right  EQU 6
 dir_down_right EQU 7
 dir_center EQU 8; никуда не двигаем =)
-
-; действия
-do_end   EQU #00
-do_stand EQU #01
-; do_get EQU #02
-; do_drop EQU #3
-; do_look  EQU #02
-; do_use   EQU #04
-; do_smach EQU #05
