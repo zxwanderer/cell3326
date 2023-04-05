6ea409a0c32a089cf93c43ec84028ffdf269151f
zxwanderer <no@mail.com>
2023-04-05 11:27:46 +0300

    updates

--- src/cell_types_data.asm
+++ src/cell_types_data.asm
@@ -1,8 +1,8 @@
 CELL_TYPES:
   DUP 256
-    dw Empty_cell_name_ptr
-    dw 00
-    ; CellType Empty_cell_name, no_way_script
+    ; dw Empty_cell_name_ptr
+    ; dw 00
+    CellType Empty_cell_name, no_way_script
     ; defb 00
   EDUP
 
