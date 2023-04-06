d0cfeedc1263c580057033397a53080b231d4ee0
zxwanderer <no@mail.com>
2023-04-06 13:34:19 +0300

    updates

--- assets/cells/floors.asm
+++ assets/cells/floors.asm
@@ -1,7 +1,15 @@
 
+Floor_3.spr equ 3
 Floor_4.spr equ 4
+Floor_8.spr equ 8
+Floor_22.spr equ 22
+Floor_23.spr equ 23
 
+  SETUP_CELL_TYPE_N Floor_3.spr, Floor_cell_name, Floor_actions
   SETUP_CELL_TYPE_N Floor_4.spr, Floor_cell_name, Floor_actions
+  SETUP_CELL_TYPE_N Floor_8.spr, Floor_cell_name, Floor_actions
+  SETUP_CELL_TYPE_N Floor_22.spr, Floor_cell_name, Floor_actions
+  SETUP_CELL_TYPE_N Floor_23.spr, Floor_cell_name, Floor_actions
 
 Floor_actions:
   db do_stand
