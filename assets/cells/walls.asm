7f5703c38a22e26a971d2bf4ec23679200a5dc36
zxwanderer <no@mail.com>
2023-04-07 20:39:24 +0300

    updates

--- assets/cells/walls.asm
+++ assets/cells/walls.asm
@@ -13,12 +13,6 @@ Soft_wall_r_down_122.spr equ 122
 
 Soft_wall_break_77.spr equ 77
 
-Wall_76.spr equ 76
-Wall_78.spr equ 78
-
-Wall_l_75.spr equ 75
-Wall_r_79.spr equ 79
-
   SETUP_CELL_TYPE_N Soft_wall_6.spr, Soft_wall_name, Soft_wall_actions
   SETUP_CELL_TYPE_N Soft_wall_87.spr, Soft_wall_name, Soft_wall_actions
   SETUP_CELL_TYPE_N Soft_wall_88.spr, Soft_wall_name, Soft_wall_actions
@@ -33,12 +27,6 @@ Wall_r_79.spr equ 79
   SETUP_CELL_TYPE_N Soft_wall_r_down_122.spr, Soft_wall_name, Soft_wall_r_down_actions
 
   SETUP_CELL_TYPE_N Soft_wall_break_77.spr, Soft_wall_name, Soft_wall_break_actions
-
-  SETUP_CELL_TYPE_N Wall_76.spr, Wall_cell_name, Wall_actions
-  SETUP_CELL_TYPE_N Wall_78.spr, Wall_cell_name, Soft_wall_actions
-
-  SETUP_CELL_TYPE_N Wall_l_75.spr, Wall_cell_name, Wall_l_actions
-  SETUP_CELL_TYPE_N Wall_r_79.spr, Wall_cell_name, Wall_r_actions
 
 Soft_wall_actions:
   defb 0
@@ -64,11 +52,30 @@ Soft_wall_r_down_actions:
 Soft_wall_break_actions:
   defb 0
 
+
+Wall_76.spr equ 76
+Wall_78.spr equ 78
+
+Wall_l_143.spr equ 143
+Wall_r_95.spr equ 95
+
+Wall_l_up_75.spr equ 75
+Wall_r_up_79.spr equ 79
+
+Wall_l_down_123.spr equ 123
+Wall_r_down_127.spr equ 127
+
+  SETUP_CELL_TYPE_N Wall_76.spr, Wall_cell_name, Wall_actions
+  SETUP_CELL_TYPE_N Wall_78.spr, Wall_cell_name, Wall_actions
+
+  SETUP_CELL_TYPE_N Wall_l_143.spr, Wall_cell_name, Wall_actions
+  SETUP_CELL_TYPE_N Wall_r_95.spr, Wall_cell_name, Wall_actions
+
+  SETUP_CELL_TYPE_N Wall_l_up_75.spr, Wall_cell_name, Wall_actions
+  SETUP_CELL_TYPE_N Wall_r_up_79.spr, Wall_cell_name, Wall_actions
+
+  SETUP_CELL_TYPE_N Wall_l_down_123.spr, Wall_cell_name, Wall_actions
+  SETUP_CELL_TYPE_N Wall_r_down_127.spr, Wall_cell_name, Wall_actions
+
 Wall_actions:
-  defb 0
-
-Wall_l_actions:
-  defb 0  
-
-Wall_r_actions:
   defb 0
