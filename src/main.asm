f2758fe593b775d0e192400ed9176c9fa513e224
zxwanderer <no@mail.com>
2023-04-08 14:54:30 +0300

    update

--- src/main.asm
+++ src/main.asm
@@ -45,9 +45,5 @@ display 'INT_TABLE: ', INT_TABLE, "-", INT_TABLE+256-1
 display 'INT_VECTOR:', INT_VECTOR, "-", INT_VECTOR
 display '[Free]     ', _all_end, '-', #FFFF, ', size: ', /D, 0x10000 - _all_end
 
-
-display 'Cells.call_cell_script', Cells.call_cell_script
-display 'Floor_actions', Floor_actions
-
 	savesna SNA_FILENAME, PROGRAM_ORG
   LABELSLIST labels.txt
