bf84d934e15f3eb939887c4340d7a22931bc3250
zxwanderer <no@mail.com>
2023-04-08 14:23:32 +0300

    updates

--- assets/cells/computers.asm
+++ assets/cells/computers.asm
@@ -1,10 +1,10 @@
 Computer_4.spr equ 4
-Computer.spr equ 20
-Computer_break.spr equ 36
+ComputerOffline_20.spr equ 20
+Computer_break_36.spr equ 36
 
   SETUP_CELL_TYPE_N Computer_4.spr, Computer_cell_name, Computer_online_actions
-  SETUP_CELL_TYPE_N Computer.spr, Computer_cell_name, Computer_actions
-  SETUP_CELL_TYPE_N Computer_break.spr, Computer_break_cell_name, Computer_break_actions
+  SETUP_CELL_TYPE_N ComputerOffline_20.spr, Computer_cell_name, Computer_actions
+  SETUP_CELL_TYPE_N Computer_break_36.spr, Computer_break_cell_name, Computer_break_actions
 
 Computer_online_actions:
   defb 0
