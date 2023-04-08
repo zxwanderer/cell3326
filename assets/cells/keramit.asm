fc89cba44c9288e2ac43803491f35a9c7e654d83
zxwanderer <no@mail.com>
2023-04-08 09:57:28 +0300

    update

--- assets/cells/keramit.asm
+++ assets/cells/keramit.asm
@@ -1,8 +1,38 @@
+Keramit_137.spr equ 137
+Keramit_138.spr equ 138
+Keramit_142.spr equ 142
+Keramit_154.spr equ 154
+Keramit_155.spr equ 155
 Keramit_170.spr equ 170
+Keramit_171.spr equ 171
+Keramit_172.spr equ 172
+Keramit_173.spr equ 173
+Keramit_175.spr equ 175
 Keramit_156.spr equ 156
+Keramit_157.spr equ 157
+Keramit_185.spr equ 185
+Keramit_186.spr equ 186
+Keramit_187.spr equ 187
+Keramit_188.spr equ 188
+Keramit_189.spr equ 189
 
+  SETUP_CELL_TYPE_N Keramit_137.spr, Keramit_cell_name, Keramit_actions
+  SETUP_CELL_TYPE_N Keramit_138.spr, Keramit_cell_name, Keramit_actions
+  SETUP_CELL_TYPE_N Keramit_142.spr, Keramit_cell_name, Keramit_actions
+  SETUP_CELL_TYPE_N Keramit_154.spr, Keramit_cell_name, Keramit_actions
+  SETUP_CELL_TYPE_N Keramit_155.spr, Keramit_cell_name, Keramit_actions
   SETUP_CELL_TYPE_N Keramit_170.spr, Keramit_cell_name, Keramit_actions
+  SETUP_CELL_TYPE_N Keramit_171.spr, Keramit_cell_name, Keramit_actions
+  SETUP_CELL_TYPE_N Keramit_172.spr, Keramit_cell_name, Keramit_actions
+  SETUP_CELL_TYPE_N Keramit_173.spr, Keramit_cell_name, Keramit_actions
+  SETUP_CELL_TYPE_N Keramit_175.spr, Keramit_cell_name, Keramit_actions
   SETUP_CELL_TYPE_N Keramit_156.spr, Keramit_cell_name, Keramit_actions
+  SETUP_CELL_TYPE_N Keramit_157.spr, Keramit_cell_name, Keramit_actions
+  SETUP_CELL_TYPE_N Keramit_185.spr, Keramit_cell_name, Keramit_actions
+  SETUP_CELL_TYPE_N Keramit_186.spr, Keramit_cell_name, Keramit_actions
+  SETUP_CELL_TYPE_N Keramit_187.spr, Keramit_cell_name, Keramit_actions
+  SETUP_CELL_TYPE_N Keramit_188.spr, Keramit_cell_name, Keramit_actions
+  SETUP_CELL_TYPE_N Keramit_189.spr, Keramit_cell_name, Keramit_actions
 
 Keramit_actions:
   defb 0
