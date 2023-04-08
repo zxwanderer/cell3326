00b35d1c5059c09d4b794e1a6941839fefc9fcac
zxwanderer <no@mail.com>
2023-04-08 20:12:47 +0300

    update

--- src/logic/Actions_h.asm
+++ src/logic/Actions_h.asm
@@ -1,8 +1,10 @@
 ; действия
 do_end   EQU #00
 do_stand EQU #01
-do_get EQU #02
-; do_drop EQU #3
+do_use EQU #02
+do_get EQU #03
+do_drop EQU #04
+
 ; do_look  EQU #02
 ; do_use   EQU #04
 ; do_smach EQU #05
