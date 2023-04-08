9f7cbb2b98f489e50fe487ef89092910f22cacf2
zxwanderer <no@mail.com>
2023-04-08 09:10:13 +0300

    updates

--- assets/cells/walls.asm
+++ assets/cells/walls.asm
@@ -2,6 +2,10 @@ Soft_wall_6.spr equ 6
 Soft_wall_87.spr equ 87
 Soft_wall_88.spr equ 88
 
+Soft_wall_L_89.spr equ 89
+Soft_wall_L2_120.spr equ 120
+Soft_wall_L3_136.spr equ 136
+
 Soft_wall_l_102.spr equ 102
 Soft_wall_r_106.spr equ 106
 
@@ -26,7 +30,11 @@ Soft_wall_break_77.spr equ 77
   SETUP_CELL_TYPE_N Soft_wall_l_down_119.spr, Soft_wall_name, Soft_wall_l_down_actions
   SETUP_CELL_TYPE_N Soft_wall_r_down_122.spr, Soft_wall_name, Soft_wall_r_down_actions
 
-  SETUP_CELL_TYPE_N Soft_wall_break_77.spr, Soft_wall_name, Soft_wall_break_actions
+  SETUP_CELL_TYPE_N Soft_wall_r_down_122.spr, Soft_wall_name, Soft_wall_r_down_actions
+
+  SETUP_CELL_TYPE_N Soft_wall_L_89.spr, Soft_wall_name, Soft_wall_L_actions
+  SETUP_CELL_TYPE_N Soft_wall_L2_120.spr, Soft_wall_name, Soft_wall_L2_actions
+  SETUP_CELL_TYPE_N Soft_wall_L3_136.spr, Soft_wall_name, Soft_wall_L3_actions
 
 Soft_wall_actions:
   defb 0
@@ -52,6 +60,14 @@ Soft_wall_r_down_actions:
 Soft_wall_break_actions:
   defb 0
 
+Soft_wall_L_actions:
+  defb 00
+
+Soft_wall_L2_actions:
+  defb 00
+
+Soft_wall_L3_actions:
+  defb 00
 
 Wall_76.spr equ 76
 Wall_78.spr equ 78
