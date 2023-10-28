1f199e7fa0b69db674b44ab0110b601856330719
zxwanderer <no@mail.com>
2023-10-28 19:36:23 +0300

    updates

--- src/parts.asm
+++ src/parts.asm
@@ -3,9 +3,22 @@
 
 ORG PROGRAM_ORG
 
-incbin "static.bin"
+static:
+  incbin "static.bin"
+static_end equ $-1
 
-incbin "dynamic.bin"
+dynamic:
+  incbin "dynamic.bin"
+dynamic_end equ $-1
+
+_all_end equ $
+
+display 'PROGRAM_ORG: ', PROGRAM_ORG
+display '-----------------------------------------'
+display 'Static:    ', static, '-', static_end,', size: ', /D, static_end - static
+display 'Dynamic:   ', dynamic, '-', dynamic_end, ', size: ', /D, dynamic_end - dynamic
+display '[Free]     ', _all_end, '-', #FFFF, ', size: ', /D, 0x10000 - _all_end
+
 
 	savesna SNA_FILENAME, PROGRAM_ORG
   LABELSLIST labels.txt
