efd1b36ea8329410230fc621b3c6b18792f6960c
zxwanderer <no@mail.com>
2024-02-16 20:53:14 +0300

    some updates

--- src/code.asm
+++ src/code.asm
@@ -1,5 +1,7 @@
   include "_defines_h.asm"
   include "../zx-core/libs/defines_h.asm"
+  include "../zx-core/libs/keyboard/define_key_h.asm"
+  include "../zx-core/libs/keyboard/scan_keys.asm"
   include "../zx-core/libs/im2/im2_lite_h.asm"
 
   ; define SHOW_START_MENU
@@ -27,20 +29,67 @@ init:
 
   ENDIF
 
-  EI
-
   CALL Hero.initHeroes
+  CALL Hero.show_hero_at_screen
+  CALL Hero.hero_look_at_cell
 
-.loop
-  halt
-  ld a, 1
-  out (#fe), a
+loop:
+  ; HALT
 
-  halt
-  ld a, 2
-  out (#fe), a
+  LD HL, keyMappingTable
+  CALL KEYBOARD_SCAN_KEYS
+  JP Z, loop
 
-  jp .loop
+  PUSH DE
+  POP HL
+GOTO_HL:
+  JP (HL)
+
+keyMappingTable:
+  DefineKey KEY_G,     PRESS_RESTART
+  DefineKey KEY_Q,     PRESS_BUTTON_UP
+  DefineKey KEY_A,     PRESS_BUTTON_DOWN
+  DefineKey KEY_O,     PRESS_BUTTON_LEFT
+  DefineKey KEY_P,     PRESS_BUTTON_RIGHT
+  DefineKey KEY_M,     PRESS_BUTTON_FIRE
+  DefineKey KEY_SPACE, PRESS_BUTTON_FIRE
+  DefineKey KEY_ENTER, PRESS_BUTTON_FIRE
+  defb _endByte
+
+PRESS_BUTTON_UP:
+  LD B, dir_up
+  JP hero_move_processing
+
+PRESS_BUTTON_DOWN:
+  LD B, dir_down
+  JP hero_move_processing
+
+PRESS_BUTTON_LEFT:
+  LD B, dir_left
+  JP hero_move_processing
+
+PRESS_BUTTON_RIGHT:
+  LD B, dir_right
+  JP hero_move_processing
+
+PRESS_BUTTON_FIRE:
+  ; CALL KEYBOARD_WAIT_UNPRESS_LONG
+  ; LD A, D
+  ; CP #50
+  ; JP C, loop
+  LD A, do_use
+  CALL Hero.do
+  JP loop
+
+PRESS_RESTART:
+  ; LD A, 6
+  ; OUT (#FE),A
+  JP loop
+
+hero_move_processing:
+  CALL Hero.move
+  CALL Hero.hero_look_at_cell
+  JP loop
 
   include "../zx-core/libs/screen/set_colors.asm"
   include "../zx-core/libs/screen/calc_down_pos.asm"
