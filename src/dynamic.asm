eac3f625a68b428436095af3af77410bf414fe53
zxwanderer <no@mail.com>
2024-02-18 19:59:56 +0300

    some updates

--- src/dynamic.asm
+++ src/dynamic.asm
@@ -1,18 +1,25 @@
 MAP_DATA:
   incbin "map.bin"
-MAP_DATA_END equ $-1
+MAP_DATA_END:
 
 MAP_MASK:
   DUP TILE_MAP_SIZE_WIDTH * TILE_MAP_SIZE_HEIGHT
   defb #00
   EDUP
-MAP_MASK_END equ $-1
+MAP_MASK_END:
 
 VIEW_BUFFER:
 	include "../zx-core/libs/tiles16/view_buffer.asm"
-VIEW_BUFFER_END equ $-1
+VIEW_BUFFER_END:
 
   include "heroes_data.asm"
-  
-display 'VIEW_BUFFER: ', VIEW_BUFFER
-display 'MAP_DATA: ', MAP_DATA
+
+  MACRO DISPLAY_SIZE name, item
+  display name, ' ', item,      '-', item_END    -1, ', size: ' , /D, item_END - item
+  ENDM
+
+  display '--- Dynamic --------------------------------------'
+  DISPLAY_SIZE 'MAP_DATA:    ', MAP_DATA
+  DISPLAY_SIZE 'MAP_MASK:    ', MAP_MASK
+  DISPLAY_SIZE 'VIEW_BUFFER: ', VIEW_BUFFER
+  display '--------------------------------------------------'
