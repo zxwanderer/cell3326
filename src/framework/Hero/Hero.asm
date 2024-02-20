3eb61cf8c08d45a26526180598700d659bd3c070
zxwanderer <no@mail.com>
2024-02-20 11:32:12 +0300

    some updates

--- src/framework/Hero/Hero.asm
+++ src/framework/Hero/Hero.asm
@@ -20,27 +20,6 @@
 ; LOGIC_ACTIVE_HERO_NUM - текущий номер героя
 ; LOGIC_ACTIVE_MAP_POS - координаты текущего события на карте
 
-
-; --------------------------------------------------------------------------------------
-; Переход на первого персонажа
-;   На выходе:
-;     LOGIC_ACTIVE_HERO_PTR - установлен на первого героя
-;     LOGIC_ACTIVE_HERO_NUM - установлен в 0
-; --------------------------------------------------------------------------------------
-firstChar:
-  LD HL, HEROES_SET
-  ld (LOGIC_ACTIVE_HERO_PTR), HL
-  XOR A
-  LD (LOGIC_ACTIVE_HERO_NUM), A
-  RET
-
-LOGIC_LAST_ACTION db 00
-LOGIC_ACTIVE_HERO_NUM db 00
-LOGIC_ACTIVE_HERO_PTR dw 0000
-LOGIC_ACTIVE_MAP_POS dw 0000
-LOGIC_ACTIVE_MAP_PTR dw 0000
-LOGIC_VIEW_POS dw 00
-
 ; --------------------------------------------------------------------------------------
 ; Движение или поворот текущего персонажа
 ; Вход:
