a5851918b4afbc21cad23eeb6ee6807f6dd1b36b
zxwanderer <no@mail.com>
2025-02-09 20:16:57 +0300

    some updates

--- src/main.asm
+++ src/main.asm
@@ -31,17 +31,22 @@ stack_end: ; #FFFF
 
 display 'PROGRAM_ORG: ', PROGRAM_ORG
 display '--------------------------------------------------'
-display 'Static:       ', static, '-', static_end,', size: ', /D, static_end - static
-display '--Code:       ', code_start, '-', code_end-1,', size: ', /D, code_end - code_start
-display '--Data:       ', data_start, '-', data_end-1,', size: ', /D, data_end - data_start
-display 'CELLS_SET:    ', CELLS_SET, '-', CELLS_SET_END-1,', size: ', /D, CELLS_SET_END-CELLS_SET
-display 'FX_SET:       ', FX_SET, '-', FX_SET_END-1,', size: ', /D, FX_SET_END-FX_SET
-display 'TEXT_SET:     ', TEXT_SET, '-', TEXT_SET_END-1,', size: ', /D, TEXT_SET_END-TEXT_SET
-display 'TRITONE:      ', TRITONE, '-', TRITONE_END-1,', size: ', /D, TRITONE_END-TRITONE
-display 'MUSIC:        ', BEEPER_MUSIC, '-', BEEPER_MUSIC_END-1,', size: ', /D, BEEPER_MUSIC_END-BEEPER_MUSIC
-display '[Free]        ', p68_font_before, '-', p68_font, ', size: ', /D, p68_font - p68_font_before
-display 'FONT_SET:     ', p68_font, '-', p68_font_end, ', size: ', /D, p68_font_end - p68_font
-display 'Dynamic:      ', dynamic, '-', dynamic_end, ', size: ', /D, dynamic_end - dynamic
+display 'Static - - - - ', static, '-', static_end,', size: ', /D, static_end - static
+display '- Code:        ', code_start, '-', code_end-1,', size: ', /D, code_end - code_start
+display '--Data:        ', data_start, '-', data_end-1,', size: ', /D, data_end - data_start
+display ' CELLS_SET:    ', CELLS_SET, '-', CELLS_SET_END-1,', size: ', /D, CELLS_SET_END-CELLS_SET
+display ' FX_SET:       ', FX_SET, '-', FX_SET_END-1,', size: ', /D, FX_SET_END-FX_SET
+display ' TEXT_SET:     ', TEXT_SET, '-', TEXT_SET_END-1,', size: ', /D, TEXT_SET_END-TEXT_SET
+display ' TRITONE:      ', TRITONE, '-', TRITONE_END-1,', size: ', /D, TRITONE_END-TRITONE
+display ' MUSIC:        ', BEEPER_MUSIC, '-', BEEPER_MUSIC_END-1,', size: ', /D, BEEPER_MUSIC_END-BEEPER_MUSIC
+display '[Free]         ', p68_font_before, '-', p68_font, ', size: ', /D, p68_font - p68_font_before
+display ' FONT_SET:     ', p68_font, '-', p68_font_end, ', size: ', /D, p68_font_end - p68_font
+display 'Dynamic - - - -', dynamic, '-', dynamic_end, ', size: ', /D, dynamic_end - dynamic
+display ' MAP_DATA:     ', MAP_DATA, '-', MAP_DATA_END-1,', size: ', /D, MAP_DATA_END-MAP_DATA
+display ' MAP_MASK:     ', MAP_MASK, '-', MAP_MASK_END-1,', size: ', /D, MAP_MASK_END-MAP_MASK
+display ' VIEW_BUFFER:  ', VIEW_BUFFER, '-', VIEW_BUFFER_END-1,', size: ', /D, VIEW_BUFFER_END-VIEW_BUFFER
+display ' HEROES_DATA:  ', HEROES_DATA, '-', HEROES_DATA_END-1,', size: ', /D, HEROES_DATA_END-HEROES_DATA
+
 ; display '[Free]     ', dynamic_end+1, '-', INT_TABLE-1, ', size: ', /D, INT_TABLE - dynamic_end
 
 display '---Interrupt---------------------------------------'
